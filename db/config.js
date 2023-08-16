e3a03c026a8acbea8f80e644a086a30f9abddecf
Juan David <judaram98@gmail.com>
2023-08-16 13:23:54 -0600

    Migraciones para crear y agregar columnas a una tabla

--- db/config.js
+++ db/config.js
@@ -2,8 +2,8 @@ const { config } = require('./../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,17 +5,17 @@ const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  // dialect: 'postgres',
-  dialect: 'mysql',
+  dialect: 'postgres',
+  // dialect: 'mysql',
   logging: true,
 });
 
 setupModels(sequelize);
 
-sequelize.sync();
+// sequelize.sync();
 
 module.exports = sequelize;
