e14ba2dd8ec25b5244fc0fb77ec2e2e24b59345e
Juan David <judaram98@gmail.com>
2023-08-16 16:31:14 -0600

    Relacion uno a muchos

---
+++ db/models/product.model.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const { CATEGORY_TABLE } = require('./category.model');
+
+const PRODUCT_TABLE = 'products';
+const ProductSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  image: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  description: {
+    allowNull: false,
+    type: DataTypes.TEXT,
+  },
+  price: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Sequelize.NOW,
+  },
+  //FK
+  categoryId: {
+    field: 'category_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: CATEGORY_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelet: 'SET NULL',
+  },
+};
+
+class Product extends Model {
+  // static permite que los metodos sean llamados sin necesidad de una instancia.
+  static associate(models) {
+    this.belongsTo(models.Category, { as: 'categories' });
+  }
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: PRODUCT_TABLE,
+      modelName: 'Product',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { PRODUCT_TABLE, ProductSchema, Product };
